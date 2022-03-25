d982d4ecf1bbe5978d32c45b67473e46542823b0
SiarheiTamkovich <mpg2000@mail.ru>
2022-03-25 09:57:30 +0300

    feat: create components

--- ng-app/src/app/app.module.ts
+++ ng-app/src/app/app.module.ts
@@ -4,9 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { CardComponent } from './main/card/card.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, MainComponent, CardComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
