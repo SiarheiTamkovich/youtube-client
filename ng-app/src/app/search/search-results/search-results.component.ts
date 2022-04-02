39240fef7c96b006d86eed95508b7c60fe32defb
SiarheiTamkovich <mpg2000@mail.ru>
2022-04-02 09:46:29 +0300

    refactor: add unsubscribe metod to getting data

--- ng-app/src/app/search/search-results/search-results.component.ts
+++ ng-app/src/app/search/search-results/search-results.component.ts
@@ -1,25 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { SearchItemModel } from '../search-item.model';
 import { SearchResponseModel } from '../search-response.model';
 import { SearchService } from '../search.service';
+import { Subscription } from 'rxjs';
+
 
 @Component({
   selector: 'app-search-results',
   templateUrl: './search-results.component.html',
   styleUrls: ['./search-results.component.scss'],
 })
-export class SearchResultsComponent implements OnInit {
+export class SearchResultsComponent implements OnInit, OnDestroy {
 
   public videoResponse: SearchResponseModel;
   public videoItems: SearchItemModel[] = [];
+  public sortByDate: boolean = true;
+
+  private _dataSubscription: Subscription;
 
   constructor(private searchService: SearchService) {}
 
-  ngOnInit() {
-    this.searchService.getData().subscribe((data: SearchResponseModel) => {
+  ngOnInit(): void {
+    this._dataSubscription = this.searchService.getData$().subscribe((data: SearchResponseModel) => {
       this.videoResponse = data;
       this.videoItems = data.items;
-      console.log(this.videoItems[0])
+      console.log(this.videoItems[0].snippet.publishedAt)
     });
   }
+
+  ngOnDestroy(): void {
+    this._dataSubscription.unsubscribe();
+  }
+
 }
