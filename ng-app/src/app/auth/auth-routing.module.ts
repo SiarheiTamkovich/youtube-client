219880ba0d3bda90fde3d614ba7fbc6d96c97a3c
SiarheiTamkovich <mpg2000@mail.ru>
2022-04-08 15:28:52 +0300

    feat: add login page styles

--- ng-app/src/app/auth/auth-routing.module.ts
+++ ng-app/src/app/auth/auth-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { LogoutComponent } from './components/logout/logout.component';
-import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
+import { LogoutComponent } from './pages/logout/logout.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full'},
