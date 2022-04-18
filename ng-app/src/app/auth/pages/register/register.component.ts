887450cb49f16d77a3f2aa8e574afac8d9944036
SiarheiTamkovich <mpg2000@mail.ru>
2022-04-18 14:22:50 +0300

    feat: add email validation form

--- ng-app/src/app/auth/pages/register/register.component.ts
+++ ng-app/src/app/auth/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  public userForm: FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
+
+    this.userForm = new FormGroup ({
+      firstName: new FormControl('', [Validators.required]),
+      lastName: new FormControl('',  [Validators.required]),
+      mail: new FormControl('', [
+        Validators.required,
+        Validators.email,
+      ])
+    });
+
+  }
+
+  get firstName() {
+    return this.userForm.get('firstName');
+  }
+  get lastName() {
+    return this.userForm.get('lastName');
+  }
+  get mail() {
+    return this.userForm.get('mail');
+  }
+
+  public submit() {
+    console.log(this.userForm)
   }
 
 }
