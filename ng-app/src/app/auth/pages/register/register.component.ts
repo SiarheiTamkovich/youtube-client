3bd4004290a459db0094f8bb485a423ebfe074c7
SiarheiTamkovich <mpg2000@mail.ru>
2022-04-18 22:25:56 +0300

    feat: add validators to password

--- ng-app/src/app/auth/pages/register/register.component.ts
+++ ng-app/src/app/auth/pages/register/register.component.ts
@@ -21,7 +21,9 @@ export class RegisterComponent implements OnInit {
       'mail': new FormControl('', [Validators.required,Validators.email]),
       'password': new FormControl('', [
         Validators.required,
-        Validators.minLength(8),
+        Validators.pattern(
+          '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*?[#?!@$%^&*-])[a-zA-Z0-9].{7,}$'
+        ),
       ]),
     });
   }
